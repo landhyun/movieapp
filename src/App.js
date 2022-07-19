ceec8fc6292c74fc2be8bd269f13080c21241644
landhyun <wlgusdbr22@naver.com>
2022-07-19 20:25:17 +0900

    #6.4 cleanUp fin

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
+function Hello() {
+    function effectFn() {
+        console.log("생성됨. 해당 function의 return 요소를 보여줌");
+        return () => console.log("사라짐. 여기서 return을 한번 더 해주어 반대가 되었기 때문에 해당 function의 return 요소가 없음");
+    }
+    useEffect(effectFn, [])
+    return <h1>Hello</h1>;
+}
+
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [keyword, setKeyword] = useState("");
-
-    const onClick = () => setCounter((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-
-    useEffect(() => {
-        console.log("call the API (한 번만 실행되는 함수)")
-    }, []);
-    useEffect(() => {
-        if (keyword !== "" && keyword.length > 5) {
-            console.log("keyword가 변화할 때")}
-    }, [keyword]);
-    useEffect(() => {
-      console.log("counter가 변화할 때");
-    }, [counter]);
-
+    const [show, setShow] = useState(false);
+    const onClick = () => {
+        setShow((prev) => !prev);
+    }
     return (
         <div>
-        <input value={keyword} onChange={onChange} type="text" placeholder="Search here"></input>
-        <h3>{counter}</h3>
-        <button onClick={onClick}>click</button>
-      </div>
+            <button onClick={onClick}>{show ? "hide" : "show"}</button>   
+            {show ? <Hello/> : null}
+        </div>
     );
 }
 
