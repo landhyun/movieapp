3741cfa3a3dd87e769f5070a7203f5ffd15d3ae9
landhyun <wlgusdbr22@naver.com>
2022-07-20 17:52:13 +0900

    #7.3 await

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-    const [] = useState(true);
-    const [coins, setCoins] = useState([]);
-    useEffect(() => {}, [])
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => { 
+        /* const json 선언 과정
+        const response = await fetch(
+            "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year");
+        const json = await response.json();
+        */
+        
+        const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")).json();
+        setMovies(json.data.movies);
+        setLoading(!loading);
+    }
+    
+    /* useEffect()를 이용한 api 호출
+    useEffect(() => {
+        fetch(
+          "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            setMovies(json.data.movies);
+            setLoading(!loading);
+          });
+    }, [])
+    */
+    
+    console.log(movies);
     return (
-      <>
-      </>
+        <>
+            {loading ? <h1>Loading...</h1> : null}
+        </>
     );
 }
 
--- src/Coin.js
+++ src/Coin.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
 function Coin() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(!loading);
-      });
-  }, []);
-  return (
-    <>
-    <h1>코인 {loading ? "" : `(${coins.length})`}</h1>
-    {loading ? ( <strong>Loading...</strong> ) : (
-    <select>
-        {coins.map((coin) => (
-        <option>
-            {coin.name}({coin.symbol}) : ${Math.round(coin.quotes.USD.price)}
-        </option>
-        ))}
-    </select>
-    )}
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(!loading);
+            });
+    }, []);
+    return (
+        <>
+        <h1>코인 {loading ? "" : `(${coins.length})`}</h1>
+        {loading ? (<strong>Loading...</strong>) : (
+            <select>
+                { coins.map((coin) => (
+                    <option>
+                        {coin.name}({coin.symbol}) : ${Math.round(coin.quotes.USD.price)}
+                    </option>))
+                }
+            </select>
+        )}
     </>
   );
 }
